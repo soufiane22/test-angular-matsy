9ee0d76badee0f61956955883f6e8c0f625d3e46
Soufiane ALOUI EL IDRISSI <tanger.soufian2015@gmail.com>
2024-07-11 11:17:22 +0100

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-angular-matsy';
+  price!: number;
+  total: number = 0;
+  listPrice: number[] = [];
+  sommePayee : number = 0;
+  monnaie : string [] = [];
+
+  addPrice(){
+    console.log("price ",this.price);
+    this.listPrice.push(this.price);
+    this.total += this.price;
+    this.price = 0;
+  }
+
+  calculerMonnaieArendre(){
+    let monnaieARendre = this.sommePayee - this.total;
+     this.monnaie = [];
+
+    while (monnaieARendre >= 10) {
+      this.monnaie.push('10 Euros');
+      monnaieARendre -= 10;
+    }
+    while (monnaieARendre >= 5) {
+      this.monnaie.push('5 Euros');
+      monnaieARendre -= 5;
+    }
+    while (monnaieARendre >= 1) {
+      this.monnaie.push('1 Euro');
+      monnaieARendre -= 1;
+    }
+  }
+
 }
